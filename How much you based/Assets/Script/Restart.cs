5b88611e6e3ded830e9d088e9343b5df0b92f7f6
Vladislav <86968923+VladGavrilchenko@users.noreply.github.com>
2022-08-11 19:25:57 +0300

    add advertising

--- How much you based/Assets/Script/Restart.cs
+++ How much you based/Assets/Script/Restart.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Restart : MonoBehaviour
 {
+
+    [SerializeField] private Button restartButton;
+    public void RestartSceneButton()
+    {
+        AdsManager.Instance.ShowAd(this);
+        restartButton.interactable = false;
+    }
+
     public void RestartScene()
     {
         int currentSceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
